2017163173d08f543ff210e5be0c21cac7d98e14
이석 <buddah99@naver.com>
2022-07-09 10:21:29 +0900

    BaseActivity

--- app/src/main/java/com/nepplus/myapplication/MainActivity.kt
+++ app/src/main/java/com/nepplus/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.nepplus.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
